f33c94f28a1f4d996b0d320fc7387102ce2b9f9b
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-01-07 03:12:51 +0600

    first commit

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+var typingEffect = new Typed(".auto-type",{
+  strings : ["Font-end Devoloper","Quick Learner","Researcher","SEO Expart","Designer","Self Motivator"],
+  loop : true,
+  typeSpeed : 50, 
+  backSpeed : 30,
+  backDelay : 2000
+})
+
